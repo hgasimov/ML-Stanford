79398fc3a4befa71b8c59ab6ead230010b2a826c
hgasimov <gasimov.huseyn@gmail.com>
2013-10-29 16:14:15 +0100

    remove debug lines

--- prog assignment 1/mlclass-ex1/normalEqn.m
+++ prog assignment 1/mlclass-ex1/normalEqn.m
@@ -5,18 +5,6 @@ function [theta] = normalEqn(X, y)
 
 theta = zeros(size(X, 2), 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Complete the code to compute the closed form solution
-%               to linear regression and put the result in theta.
-%
-
-% ---------------------- Sample Solution ----------------------
-
 theta = pinv(X' * X) * X' * y;
-
-% -------------------------------------------------------------
-
-
-% ============================================================
 
 end
