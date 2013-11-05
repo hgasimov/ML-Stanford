f614a69659e15ef228dbe33f328365a814fea9be
hgasimov <gasimov.huseyn@gmail.com>
2013-11-05 16:55:30 +0100

    Programming assignment 2

---
+++ prog assignment 2/sigmoid.m
@@ -0,0 +1,10 @@
+function g = sigmoid(z)
+%SIGMOID Compute sigmoid functoon
+%   J = SIGMOID(z) computes the sigmoid of z.
+
+% You need to return the following variables correctly 
+g = 1 ./ (1 + exp(-z));
+
+% =============================================================
+
+end
